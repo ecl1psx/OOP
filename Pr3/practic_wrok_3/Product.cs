8cd6841c174191da8685b66bebd83a08cd97ce73
ecl1psx <112760081+ecl1psx@users.noreply.github.com>
2023-03-04 00:22:49 +0200

    Add files via upload

---
+++ Pr3/practic_wrok_3/Product.cs
@@ -0,0 +1,8 @@
+namespace practic_wrok_3;
+
+public class Product
+{
+    public string name;
+    public int quantity;
+    public DateTime dateTime;
+}
---
+++ Pr3/practic_wrok_3/Program.cs
@@ -0,0 +1,29 @@
+﻿using practic_wrok_3;
+
+class Program
+{
+    public static void Main()
+    {
+        var csvAnalyzer = new CsvAnalyzer( Path.Combine(Environment.CurrentDirectory, $"data.csv"));
+        var dictionary = csvAnalyzer.AggregateByDate();
+        csvAnalyzer.Records = CsvFileRecord.From(dictionary);
+        
+        csvAnalyzer.Save();;
+
+        var productsList = new ProductList("/home/ecl1psx/products");
+        var products = productsList.GetProductsForDate(DateTime.Now);
+
+        foreach (var product in products)
+        {
+            ProductList.PrintProduct(product);
+        }
+
+        var imageEditor = new ImageEditor("/home/ecl1psx/test.jpg");
+        imageEditor.Reverse();
+        imageEditor.ApplyChanges();
+
+        var textStatistics = new TextFileStatistics("/home/ecl1psx/test2");
+        var report = textStatistics.CollectStatistic();
+        textStatistics.SaveReport("/home/ecl1psx/test2/report.txt", report);
+    }
+}
